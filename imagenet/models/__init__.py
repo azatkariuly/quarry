71c60254a7fc98d1386583c0f06bc692d0092fe5
azatkariuly <azat.kariuly@gmail.com>
2022-02-11 15:35:29 +0900

    initial commit

--- imagenet/models/__init__.py
+++ imagenet/models/__init__.py
@@ -1,8 +1,3 @@
-
-from .alexnet import *
-from .alexnet_binary import *
 from .resnet import *
-from .resnet_binary import *
-from .vgg_cifar10_binary import *
 from .resnet_lsq import *
 from .resnet_lsq_ps import *
