afbd027d52838539f201ac47761c862d8a3ec529
azatkariuly <azat.kariuly@gmail.com>
2022-02-12 21:27:28 +0900

    initial commit

---
+++ imagenet/main_gpu.py
@@ -0,0 +1,379 @@
+import os
+import sys
+import shutil
+import numpy as np
+import time, datetime
+import torch
+import random
+import logging
+import argparse
+import torch.nn as nn
+import torch.utils
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.utils.data.distributed
+from split import split_model_conv
+
+#sys.path.append("../")
+from utils import *
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+from birealnet import birealnet18
+
+#progress bar
+sys.path.append(os.path.join(os.path.dirname(__file__), "progress"))
+from progress.bar import Bar as Bar
+
+model_names = sorted(name for name in models.__dict__
+                     if name.islower() and not name.startswith("__")
+                     and callable(models.__dict__[name]))
+
+parser = argparse.ArgumentParser(description='PyTorch ConvNet Training')
+
+parser.add_argument('--results_dir', metavar='RESULTS_DIR', default='./results',
+                    help='results dir')
+parser.add_argument('--save', metavar='SAVE', default='',
+                    help='saved folder')
+parser.add_argument('--dataset', metavar='DATASET', default='imagenet',
+                    help='dataset name or folder')
+parser.add_argument('--model', '-a', metavar='MODEL', default='alexnet',
+                    choices=model_names,
+                    help='model architecture: ' +
+                    ' | '.join(model_names) +
+                    ' (default: alexnet)')
+parser.add_argument('--input_size', type=int, default=None,
+                    help='image input size')
+parser.add_argument('--model_config', default='',
+                    help='additional architecture configuration')
+parser.add_argument('--type', default='torch.cuda.FloatTensor',
+                    help='type of tensor - e.g torch.cuda.HalfTensor')
+parser.add_argument('--gpus', default='0',
+                    help='gpus used for training - e.g 0,1,3')
+parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
+                    help='number of data loading workers (default: 8)')
+parser.add_argument('--epochs', default=2500, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('--optimizer', default='SGD', type=str, metavar='OPT',
+                    help='optimizer function used')
+parser.add_argument('--lr', '--learning_rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--print-freq', '-p', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', type=str, metavar='FILE',
+                    help='evaluate model FILE on validation set')
+parser.add_argument('--pretrained_model', default='', type=str, metavar='FILE',
+                    help='pretrained model FILE')
+parser.add_argument('--nbits', default=3, type=int,
+                    metavar='N', help='layer quantization precision (default: 3)')
+parser.add_argument('--psumq-bits', default=3, type=int,
+                    metavar='N', help='partial sum quantization precision (default: 3)')
+#assume pretrained baseline is not splitted
+parser.add_argument('--pretrained_baseline', default='', type=str, metavar='FILE',
+                    help='pretrained baseline model FILE')
+
+if not os.path.exists('log'):
+    os.mkdir('log')
+
+log_format = '%(asctime)s %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+fh = logging.FileHandler(os.path.join('log/log.txt'))
+fh.setFormatter(logging.Formatter(log_format))
+logging.getLogger().addHandler(fh)
+
+def main():
+    if not torch.cuda.is_available():
+        sys.exit(1)
+    start_t = time.time()
+
+    if args.save is '':
+        args.save = datetime.now().strftime('/garbage')
+    save_path = os.path.join(args.results_dir, args.save)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    cudnn.benchmark = True
+    cudnn.enabled=True
+    logging.info("args = %s", args)
+
+    # create model
+    logging.info("creating model %s", args.model)
+    model = models.__dict__[args.model]
+
+    model_config = {'input_size': args.input_size, 'dataset': args.dataset,
+                    'nbits': args.nbits, 'psumq_bits': args.psumq_bits}
+
+    if args.model_config is not '':
+        model_config = dict(model_config, **literal_eval(args.model_config))
+
+    # load model
+    model = model(**model_config)
+    logging.info("created model with configuration: %s", model_config)
+    #print(model)
+    model = nn.DataParallel(model).cuda()
+
+    #load pretrained baseline model (needed to be splitted)
+    if args.pretrained_baseline:
+        if not os.path.isfile(args.pretrained_baseline):
+            parser.error('invalid file for pretrained baseline: {}'.format(args.pretrained_baseline))
+
+        model_baseline = torch.load(args.pretrained_baseline)
+
+        #split
+        split_model_conv(model, model_baseline)
+
+        print('DONE splitting and copying baseline')
+    #load pretrained model
+    elif args.pretrained_model:
+        if not os.path.isfile(args.pretrained_model):
+            parser.error('invalid file for pretrained model: {}'.format(args.pretrained_model))
+        checkpoint = torch.load(args.pretrained_model)
+        model.load_state_dict(checkpoint['state_dict'])
+        logging.info("loaded pretrained model")
+
+    criterion = nn.CrossEntropyLoss()
+    criterion = criterion.cuda()
+    criterion_smooth = CrossEntropyLabelSmooth(CLASSES, args.label_smooth)
+    criterion_smooth = criterion_smooth.cuda()
+
+    all_parameters = model.parameters()
+    weight_parameters = []
+    for pname, p in model.named_parameters():
+        if p.ndimension() == 4 or pname=='classifier.0.weight' or pname == 'classifier.0.bias':
+            weight_parameters.append(p)
+    weight_parameters_id = list(map(id, weight_parameters))
+    other_parameters = list(filter(lambda p: id(p) not in weight_parameters_id, all_parameters))
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+
+    #scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lambda step : (1.0-step/args.epochs), last_epoch=-1)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs, eta_min=0, last_epoch=-1)
+    start_epoch = 0
+    best_top1_acc= 0
+
+    # optionally resume from a checkpoint
+    if args.pretrained:
+        if not os.path.isfile(args.pretrained):
+            parser.error('invalid checkpoint: {}'.format(args.pretrained))
+
+        checkpoint = torch.load(args.pretrained)
+        model.load_state_dict(checkpoint['state_dict'], strict=False)
+
+        logging.info("loaded pretrained '%s' (epoch %s)",
+                     args.pretrained, checkpoint['epoch'])
+
+    elif args.evaluate:
+        if not os.path.isfile(args.evaluate):
+            parser.error('invalid checkpoint: {}'.format(args.evaluate))
+        checkpoint = torch.load(args.evaluate)
+        model.load_state_dict(checkpoint['state_dict'], strict=False)
+        logging.info("loaded checkpoint '%s' (epoch %s)",
+                     args.evaluate, checkpoint['epoch'])
+
+    elif args.resume:
+        checkpoint_tar = args.resume
+        if not os.path.isfile(checkpoint_tar):
+            parser.error('invalid resume checkpoint: {}'.format(checkpoint_tar))
+        checkpoint = torch.load(checkpoint_tar)
+        start_epoch = checkpoint['epoch']
+        best_top1_acc = checkpoint['best_top1_acc']
+        model.load_state_dict(checkpoint['state_dict'], strict=False)
+        logging.info("loading checkpoint '%s'", args.resume)
+
+        # adjust the learning rate according to the checkpoint
+        for epoch in range(start_epoch):
+            scheduler.step()
+
+    # load training data
+    traindir = os.path.join(args.data, 'train')
+    valdir = os.path.join(args.data, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    # data augmentation
+    crop_scale = 0.08
+    lighting_param = 0.1
+    train_transforms = transforms.Compose([
+        transforms.RandomResizedCrop(224, scale=(crop_scale, 1.0)),
+        Lighting(lighting_param),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize])
+
+    train_dataset = datasets.ImageFolder(
+        traindir,
+        transform=train_transforms)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+
+    # load validation data
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    if args.evaluate:
+        valid_obj, valid_top1_acc, valid_top5_acc = validate(0, val_loader, model, criterion, args)
+        print('Best Accuracy:', valid_top1_acc)
+        return
+
+    # train the model
+    epoch = start_epoch
+    while epoch < args.epochs:
+        train_obj, train_top1_acc,  train_top5_acc = train(epoch,  train_loader, model, criterion_smooth, optimizer, scheduler)
+        valid_obj, valid_top1_acc, valid_top5_acc = validate(epoch, val_loader, model, criterion, args)
+
+        is_best = False
+        if valid_top1_acc > best_top1_acc:
+            best_top1_acc = valid_top1_acc
+            is_best = True
+
+        save_checkpoint({
+            'epoch': epoch,
+            'state_dict': model.state_dict(),
+            'best_top1_acc': best_top1_acc,
+            'optimizer' : optimizer.state_dict(),
+            }, is_best, save_path)
+
+        print('Best Accuracy: ', best_top1_acc.item(), '\n')
+
+        epoch += 1
+
+    training_time = (time.time() - start_t) / 36000
+    print('total training time = {} hours'.format(training_time))
+
+
+def train(epoch, train_loader, model, criterion, optimizer, scheduler):
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+
+    model.train()
+    end = time.time()
+    scheduler.step()
+
+    for param_group in optimizer.param_groups:
+        cur_lr = param_group['lr']
+    print('learning_rate:', cur_lr)
+
+    bar = Bar('Processing', max=len(train_loader))
+    for i, (images, target) in enumerate(train_loader):
+        data_time.update(time.time() - end)
+        images = images.cuda()
+        target = target.cuda()
+
+        # compute outputy
+        logits = model(images)
+        loss = criterion(logits, target)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(logits, target, topk=(1, 5))
+        n = images.size(0)
+        losses.update(loss.item(), n)   #accumulated loss
+        top1.update(prec1.item(), n)
+        top5.update(prec5.item(), n)
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # plot progress
+        bar.suffix  = '{phase} - Epoch: [{epoch}]({batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss: {loss:.4f} | top1: {top1: .4f} | top5: {top5: .4f}'.format(
+                    phase='TRAINING',
+                    epoch=epoch,
+                    batch=i + 1,
+                    size=len(train_loader),
+                    data=data_time.val,
+                    bt=batch_time.val,
+                    total=bar.elapsed_td,
+                    eta=bar.eta_td,
+                    loss=losses.avg,
+                    top1=top1.avg,
+                    top5=top5.avg,
+                    )
+        bar.next()
+    bar.finish()
+
+        #progress.display(i)
+
+    return losses.avg, top1.avg, top5.avg
+
+def validate(epoch, val_loader, model, criterion, args):
+    batch_time = AverageMeter('Time', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+
+    # switch to evaluation mode
+    model.eval()
+
+    bar = Bar('Processing', max=len(val_loader))
+    with torch.no_grad():
+        end = time.time()
+        for i, (images, target) in enumerate(val_loader):
+            images = images.cuda()
+            target = target.cuda()
+
+            # compute output
+            logits = model(images)
+            loss = criterion(logits, target)
+
+            # measure accuracy and record loss
+            pred1, pred5 = accuracy(logits, target, topk=(1, 5))
+            n = images.size(0)
+            losses.update(loss.item(), n)
+            top1.update(pred1[0], n)
+            top5.update(pred5[0], n)
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            # plot progress
+            bar.suffix  = '{phase} - Epoch: [{epoch}]({batch}/{size}) | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss: {loss:.4f} | top1: {top1: .4f} | top5: {top5: .4f}'.format(
+                        phase='TEST',
+                        epoch=epoch,
+                        batch=i + 1,
+                        size=len(val_loader),
+                        bt=batch_time.val,
+                        total=bar.elapsed_td,
+                        eta=bar.eta_td,
+                        loss=losses.avg,
+                        top1=top1.avg,
+                        top5=top5.avg,
+                        )
+            bar.next()
+        bar.finish()
+
+        print(' * acc@1 {top1.avg:.3f} acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
+
+    return losses.avg, top1.avg, top5.avg
+
+
+if __name__ == '__main__':
+    main()
