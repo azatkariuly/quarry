c4709f88115a17c5e5112751220da40474bd119d
azatkariuly <azat.kariuly@gmail.com>
2022-02-17 16:57:05 +0900

    initial commit

--- imagenet/models/lsq.py
+++ imagenet/models/lsq.py
@@ -86,7 +86,7 @@ def quantizeLSQ(v, s, p, isActivation=False, k=8):
     vbar = round_pass((v/s).clamp(Qn, Qp))
     #vhat = vbar*s.round() #.clamp(0, 2**k-1).round()
     #vhat = (vbar*s.round()).detach() - (vbar*s).detach() + vbar*s
-    vhat = vbar*dsf_round_pass(s, k)
+    vhat = vbar*s #dsf_round_pass(s, k)
     return vhat
 
 class Conv2dLSQ(nn.Conv2d):
